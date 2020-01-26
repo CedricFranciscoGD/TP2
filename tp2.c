125900f628339a1ee265e2dc9ee1ff5cf973cc64
CedricFranciscoGD <franciscomca@live.fr>
2020-01-26 15:00:55 +0100

    action pokémon

--- tp2.c
+++ tp2.c
@@ -41,15 +41,17 @@ int main (){
 
 
 
-
+// Boucle de combat 
 	while (pdvAspicot > 0 && pdvSalameche > 0 )
 	{
+		//-------------------------------------------------------------------------------------------------------------------------
+		//TOUR DE SALAMECHE
 		if (pdmSalameche<10)
 		{
-			printf("Vous recuperez 1 pt de magie\n");
+			printf("Salameche recupere 1 pt de magie\n");
 			pdmSalameche ++;
 		}
-		printf("C'est votre tour que souhaitez vous faire ? \n");
+		printf("C'est le tour de Salameche que souhaitez vous faire ? \n");
 		printf("Tapez 1 pour lancer une attaque legere\n");
 		printf("Tapez 2 pour vous defendre\n");
 		printf("Tapez 3 pour lancer un sort de type poison\n");
@@ -115,9 +117,176 @@ int main (){
 				scanf("%d", &choixAction);
 			}
 		}
+		//FIN DU TOUR DE SALAMECHE 
+		//--------------------------------------------------------------------------------------------------------------------------------
+
+
+		//-------------------------------------------------------------------------------------------------------------------------
+		//TOUR DE BULBIZARRE
+		if (pdmBulbizarre<10)
+		{
+			printf("Bulbizarre recupere 1 pt de magie\n");
+			pdmBulbizarre ++;
+		}
+		printf("C'est le tour de Bulbizarre que souhaitez vous faire ? \n");
+		printf("Tapez 1 pour lancer une attaque legere\n");
+		printf("Tapez 2 pour vous defendre\n");
+		printf("Tapez 3 pour lancer un sort de type poison\n");
+		printf("Tapez 4 pour utiliser un sort d'antidote\n");
+		printf("Tapez 5 pour aggro un nouvel ennemi\n");
+		scanf("%d", &choixAction);
+		
+		if (choixAction == 1)
+		{
+			printf("Vous lancez une attaque legere sur Aspicot ! \n");
+			pdvAspicot--;
+			printf("Aspicot a %d points de vie! \n", pdvAspicot);
+		}
+		
+
+		else if (choixAction == 2)
+		{
+			printf("Vous vous protegez de Aspicot votre defense est multipliee par 4 ! \n");
+			printf("Vous avez %d point de vie ! \n", pdvBulbizarre);
+			
+		}
+
+
+		else if (choixAction == 3)
+		{
+			printf("Vous lancez un sort sur Aspicot \n");
+			pdvAspicot--;
+			printf("Il vous reste %d points de magie \n", pdmBulbizarre -= coutSort);
+			printf("Aspicot a %d points de vie! \n", pdvAspicot);
+			printf("Aspicot est empoisonne ! \n");
+			printf("Le poison fait son effet Aspicot perd 1 point de vie ! \n");
+			printf("Aspicot a %d points de vie \n", pdvAspicot -= 1 );
+		}
+
+		// N'ayant pas réussi à faire perdurer le poison, mon antidote sera un soin de 3 pv contre 5 pts de magie.
+		else if (choixAction == 4)
+		{
+			printf("Vous utilisez un sort d'antidote \n");
+			printf("Vous recuperez 3 points de vie\n");
+			printf("Vous avez %d points de vie\n", pdvBulbizarre += 3 );
+			printf("Il vous reste %d points de magie \n", pdmBulbizarre -= coutAntidote);
+
+		}
+
+		else if (choixAction == 5)
+		{	
+			if (nombreEnnemi == 1 )
+			{
+			printf("Abo apparait ! \n");
+			printf("Abo a %d points de vie et %d points de magie \n ", pdvAbo, pdmAbo);
+			nombreEnnemi++;
+			}
+			else if (nombreEnnemi == 2)
+			{
+				printf("Smogo apparait ! \n");
+				printf("Smogo a %d points de vie et %d points de magie \n ", pdvSmogo, pdmSmogo);
+				nombreEnnemi++;
+			}
+			
+			else if (nombreEnnemi == 3)
+			{
+				printf("Nombre maximum d'ennemi atteint, choisissez une autre action ! \n ");
+				scanf("%d", &choixAction);
+			}
+		}
+		//FIN DU TOUR DE BULBIZARRE 
+		//--------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+		//-------------------------------------------------------------------------------------------------------------------------
+		//TOUR DE CARAPUCE
+		if (pdmBulbizarre<10)
+		{
+			printf("Carapuce recupere 1 pt de magie\n");
+			pdmCarapuce ++;
+		}
+		printf("C'est le tour de Carapuce que souhaitez vous faire ? \n");
+		printf("Tapez 1 pour lancer une attaque legere\n");
+		printf("Tapez 2 pour vous defendre\n");
+		printf("Tapez 3 pour lancer un sort de type poison\n");
+		printf("Tapez 4 pour utiliser un sort d'antidote\n");
+		printf("Tapez 5 pour aggro un nouvel ennemi\n");
+		scanf("%d", &choixAction);
+		
+		if (choixAction == 1)
+		{
+			printf("Vous lancez une attaque legere sur Aspicot ! \n");
+			pdvAspicot--;
+			printf("Aspicot a %d points de vie! \n", pdvAspicot);
+		}
+		
+
+		else if (choixAction == 2)
+		{
+			printf("Vous vous protegez de Aspicot votre defense est multipliee par 4 ! \n");
+			printf("Vous avez %d point de vie ! \n", pdvCarapuce);
+			
+		}
+
+
+		else if (choixAction == 3)
+		{
+			printf("Vous lancez un sort sur Aspicot \n");
+			pdvAspicot--;
+			printf("Il vous reste %d points de magie \n", pdmCarapuce -= coutSort);
+			printf("Aspicot a %d points de vie! \n", pdvAspicot);
+			printf("Aspicot est empoisonne ! \n");
+			printf("Le poison fait son effet Aspicot perd 1 point de vie ! \n");
+			printf("Aspicot a %d points de vie \n", pdvAspicot -= 1 );
+		}
+
+		// N'ayant pas réussi à faire perdurer le poison, mon antidote sera un soin de 3 pv contre 5 pts de magie.
+		else if (choixAction == 4)
+		{
+			printf("Vous utilisez un sort d'antidote \n");
+			printf("Vous recuperez 3 points de vie\n");
+			printf("Vous avez %d points de vie\n", pdvCarapuce += 3 );
+			printf("Il vous reste %d points de magie \n", pdmCarapuce -= coutAntidote);
+
+		}
+
+		else if (choixAction == 5)
+		{	
+			if (nombreEnnemi == 1 )
+			{
+			printf("Abo apparait ! \n");
+			printf("Abo a %d points de vie et %d points de magie \n ", pdvAbo, pdmAbo);
+			nombreEnnemi++;
+			}
+			else if (nombreEnnemi == 2)
+			{
+				printf("Smogo apparait ! \n");
+				printf("Smogo a %d points de vie et %d points de magie \n ", pdvSmogo, pdmSmogo);
+				nombreEnnemi++;
+			}
+			
+			else if (nombreEnnemi == 3)
+			{
+				printf("Nombre maximum d'ennemi atteint, choisissez une autre action ! \n ");
+				scanf("%d", &choixAction);
+			}
+		}
+		//FIN DU TOUR DE CARAPUCE 
+		//--------------------------------------------------------------------------------------------------------------------------------
+
+
 
 // Condition qui devrait empecher Aspicot de m'envoyer un sort si sa reserve de magie est insuffisante mais qui ne marche pas.
 // Je n'arrive pas a bloquer (ce n'est pas faute d'avoir essayé différentes méthodes).
+
+
+// DEBUT TOUR ENNEMIS
+//----------------------------------------------------------------------------------------------------
+// TOUR ASPICOT 
+
 	if (pdmAspicot < coutSort)
 	{
 		printf("Aspicot lance piqure ! \n");
@@ -162,36 +331,36 @@ int main (){
 
 		
 
-			if (pdvAspicot == 0 )
+			if (pdvAspicot <= 0 )
 				{	printf("Aspicot est mort !!! \n");
 					
 				}
 
-			else if (pdvSalameche == 0)
+			else if (pdvSalameche <= 0)
 				{
 				printf("Salameche etes mort !!! \n");
 				
 				}	
 
-			else if (pdvBulbizarre == 0)
+			else if (pdvBulbizarre <= 0)
 				{
 				printf("Bulbizarre etes mort !!! \n");
 				
 				}
 
-			else if (pdvAbo == 0)
+			else if (pdvAbo <= 0)
 				{
 				printf("Abo etes mort !!! \n");
 				
 				}
 
-			else if (pdvCarapuce == 0)
+			else if (pdvCarapuce <= 0)
 				{
 				printf("Carapuce etes mort !!! \n");
 				
 				}
 				
-			else if (pdvSmogo == 0)
+			else if (pdvSmogo <= 0)
 				{
 				printf("Smogo etes mort !!! \n");
 				
